ccfcb25870c0ceabc722e58938dd48c9bbfbe3a4
jamescross <crossjames813@gmail.com>
2021-01-21 21:35:03 -0600

    commit tree 0acdcf5fd7e5e8c4e05c2dfffed5e976bd02916d

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
-end
+
+def my_each(array) # put argument(s) here
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+  end
+
+  my_each(['hi', 'hello', 'bye', 'goodbye']) do |phrase|
+       phrase
+    end
+  
